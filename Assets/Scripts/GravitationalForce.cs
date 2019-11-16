e416d5fd5b65f25ae517892d6e324e3829f994ec
MaciejSkrabski <maciej.skrabski@gmail.com>
2019-11-16 07:50:33 +0100

    Added gravitational force, woohoo!

--- Assets/Scripts/GravitationalForce.cs
+++ Assets/Scripts/GravitationalForce.cs
@@ -4,16 +4,33 @@ using UnityEngine;
 
 public class GravitationalForce : MonoBehaviour
 {
-    private GameObject player;
+
+    Rigidbody rb;
+    Rigidbody pl;
     // Start is called before the first frame update
     void Start()
     {
-        player = GameObject.Find("Player");
+        GameObject player = GameObject.Find("Player");
+        rb = GetComponent<Rigidbody>();
+
+        pl = player.GetComponent<Rigidbody>();
+        
+    }
+
+    private void FixedUpdate()
+    {
+        Attract(pl);
     }
 
     // Update is called once per frame
-    void Update()
+    void Attract(Rigidbody pl)
     {
-        
+        Vector3 direction = this.transform.position - pl.transform.position;
+        float distance = direction.magnitude;
+
+        float forceMagnitude = (rb.mass * pl.mass) / Mathf.Pow(distance, 2);
+        Vector3 force = direction.normalized * forceMagnitude;
+
+        pl.AddForce(force);
     }
 }
