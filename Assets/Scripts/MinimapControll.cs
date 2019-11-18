444b01c3ea1f09559e8eb2bd5b094c616a97b0fd
Maciej Skrabski <maciej.skrabski@gmail.com>
2019-11-18 13:22:24 +0100

    Nice minimap

---
+++ Assets/Scripts/MinimapControll.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class MinimapControll : MonoBehaviour
+{
+    public Transform target;
+    public float smoothTime = 0.3F;
+
+    public Vector3 offset;
+
+
+
+    void Update()
+    {
+        // Define a target position above and behind the target transform
+        Vector3 targetPosition = target.TransformPoint(offset);
+
+        // Smoothly move the camera towards that target position
+        transform.position = targetPosition;
+    }
+}
